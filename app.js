a75cffb3fde07088866fcdeb9e33a66890eaef33
Kirtan <kirtanapatel21@gnu.ac.in>
2024-02-06 08:42:38 +0530

    Second Version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Kirtan Patel to test pipeline and this is the second versio');
+    res.send('I have made changes after making that version');
   });
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
